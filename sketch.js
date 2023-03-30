58af2937158f24d4385beabbee7814b51751a276
AdamMarshalCooke <127203265+AdamMarshalCooke@users.noreply.github.com>
2023-03-30 09:21:21 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() { 
+  createCanvas(windowWidth, windowHeight);
+  background(204);
+} 
+
+function draw() { 
+  for (var i = 0  ;  i < touches.length  ;  i++) {
+    fill (i * 50);
+    ellipse(touches[i].x, touches[i].y, 20, 20);
+  }
+}
+
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling
+ * the page.
+ */
+function touchStarted(){
+  return false;
+}
+
+function touchMoved(){
+  return false;
+}
+
+function touchEnded(){
+  return false;
+}
